81135893ce0668e93ed02906a59bf6530735c041
Suhas Ranganath <suhasranga259@gmail.com>
2018-03-30 08:45:01 +0530

    Get input from user and log it to console

--- app.js
+++ app.js
@@ -1,11 +1,56 @@
+//Budget Controller
 var budgetController = (function(){
-    //Data 
+
 })();
 
+//UI Controller
 var UIController = (function(){
-    //UI Controll
+    //DOM Strigs : object to access and control DOM class and IDs
+    var DOMstrings = {
+        type: '.add__type',
+        desc: '.add__description',
+        value: '.add__value',
+        button: '.add__btn',
+    }
+
+    //Get values from user
+    return{
+        getInput: function(){
+            return{
+                type: document.querySelector(DOMstrings.type).value,
+                description: document.querySelector(DOMstrings.desc).value,
+                value: document.querySelector(DOMstrings.value).value,
+            }
+        },
+
+        getDOMstrings: function(){
+            return DOMstrings;
+        },
+    }
 })();
 
-var controller = (function(){
-    //Bridge
-})();
+//App Controller
+var controller = (function( UIctrl, budgetCtrl ){
+    var DOM = UIctrl.getDOMstrings();
+
+
+    var ctrlAddItem = function(){
+        //TODO
+        //1. Grab the values from the input field
+        var inputData = UIctrl.getInput();
+        console.log(inputData);
+        //2. Add it to our budget controller
+        //3. Update the UI to indicate new value.
+        //4. Calculate Budget
+        //5. Display the updated budget summary
+        
+    }
+    
+    document.querySelector(DOM.button).addEventListener('click',ctrlAddItem);
+
+    document.addEventListener('keypress', function(e){
+        if(e.keyCode === 13 || e.which === 13){
+            ctrlAddItem();
+        }
+    });
+})( UIController, budgetController );
